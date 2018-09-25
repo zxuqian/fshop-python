916e1a8a21db3a4857f48d4344643735beb9a466
Xuqian Zhang <zxuqian@gmail.com>
2018-09-25 18:54:14 +0800

    -- added some messages

--- admin/app/containers/ProductPage/messages.js
+++ admin/app/containers/ProductPage/messages.js
@@ -9,6 +9,27 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.containers.ProductPage.header',
-    defaultMessage: 'This is ProductPage container !',
+    defaultMessage: 'Product Management',
   },
+  productTitle: {
+    id: 'app.containers.ProductPage.productTitle',
+    defaultMessage: 'Title',
+  },
+  productPrice: {
+    id: 'app.containers.ProductPage.productPrice',
+    defaultMessage: 'Price',
+  },
+  productQuantity: {
+    id: 'app.containers.ProductPage.productQuantity',
+    defaultMessage: 'Quantity',
+  },
+  productDescription: {
+    id: 'app.containers.ProductPage.productDescription',
+    defaultMessage: 'Description',
+  },
+  productDetail: {
+    id: 'app.containers.ProductPage.productDetail',
+    defaultMessage: 'Detail',
+  },
+
 });
