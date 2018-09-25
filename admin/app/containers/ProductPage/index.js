96d53c547fd0526e8e9d03fa678f19b5a218c6b9
Xuqian Zhang <zxuqian@gmail.com>
2018-09-25 18:38:44 +0800

    -- product form

--- admin/app/containers/ProductPage/index.js
+++ admin/app/containers/ProductPage/index.js
@@ -30,6 +30,11 @@ import Avatar from '@material-ui/core/Avatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+import Icon from '@material-ui/core/Icon';
+import Paper from '@material-ui/core/Paper';
 
 import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -38,23 +43,41 @@ import { fetchProducts } from './actions';
 const listWidth = 350;
 
 const styles = theme => ({
-  listContainer: {
+  container: {
     position: 'fixed',
     top: theme.mixins.toolbar.minHeight,
     bottom: 0,
+    display: 'flex',
+    backgroundColor: theme.palette.background.paper,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 2,
+  },
+  listPaper: {
+    position: 'relative',
   },
   list: {
     // position: 'relative',
+    flexShrink: 0,
     width: listWidth,
     maxWidth: listWidth,
     height: '100%',
-    backgroundColor: theme.palette.background.paper,
-  }
+  },
+  form: {
+    textAlign: 'center',
+    marginTop: theme.spacing.unit * 2,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '30%',
+  },
 });
 
 /* eslint-disable react/prefer-stateless-function */
 export class ProductPage extends React.Component {
-
   componentDidMount() {
     this.props.dispatch(fetchProducts());
   }
@@ -67,29 +90,165 @@ export class ProductPage extends React.Component {
         {/* <Typography variant="title">
           Avatar with text and icon
         </Typography> */}
-        <div className={classes.listContainer}>
-          <List dense={false} className={classes.list}>
-            {products.map(product => (
-              <ListItem key={product.id}>
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={product.title}
-                  secondary={product.description}
-                />
-                <ListItemSecondaryAction>
-                  <IconButton aria-label="Delete">
-                    <DeleteIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            ))}
-          </List>
+        <div className={classes.container}>
+          <Paper elevation={8} className={classes.listPaper}>
+            <List dense={false} className={classes.list}>
+              {products.map(product => (
+                <ListItem key={product.id}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={product.title}
+                    secondary={product.description}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+            <Button
+              variant="fab"
+              color="primary"
+              aria-label="Add"
+              className={classes.fab}
+            >
+              <AddIcon />
+            </Button>
+          </Paper>
+          <form className={classes.form} noValidate autoComplete="off">
+            <TextField
+              id="standard-name"
+              label="Name"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-uncontrolled"
+              label="Uncontrolled"
+              defaultValue="foo"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              required
+              id="standard-required"
+              label="Required"
+              defaultValue="Hello World"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              error
+              id="standard-error"
+              label="Error"
+              defaultValue="Hello World"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              disabled
+              id="standard-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-password-input"
+              label="Password"
+              className={classes.textField}
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+            />
+            <TextField
+              id="standard-read-only-input"
+              label="Read Only"
+              defaultValue="Hello World"
+              className={classes.textField}
+              margin="normal"
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+            <TextField
+              id="standard-dense"
+              label="Dense"
+              className={classNames(classes.textField, classes.dense)}
+              margin="dense"
+            />
+            <TextField
+              id="standard-multiline-flexible"
+              label="Multiline"
+              multiline
+              rowsMax="4"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-multiline-static"
+              label="Multiline"
+              multiline
+              rows="4"
+              defaultValue="Default Value"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              className={classes.textField}
+              helperText="Some important text"
+              margin="normal"
+            />
+            <TextField
+              id="standard-with-placeholder"
+              label="With placeholder"
+              placeholder="Placeholder"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-textarea"
+              label="With placeholder multiline"
+              placeholder="Placeholder"
+              multiline
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-number"
+              label="Number"
+              type="number"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+            <TextField
+              id="standard-search"
+              label="Search field"
+              type="search"
+              className={classes.textField}
+              margin="normal"
+            />
+            <TextField
+              id="standard-bare"
+              className={classes.textField}
+              defaultValue="Bare"
+              margin="normal"
+            />
+          </form>
         </div>
-        </React.Fragment>
+      </React.Fragment>
       // </Grid>
     );
   }
@@ -106,7 +265,7 @@ const mapStateToProps = createStructuredSelector({
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch
+    dispatch,
   };
 }
 
